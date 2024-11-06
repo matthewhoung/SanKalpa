1d420ab9e615046aafa899f37bbf7561ccf2cf6e
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-11-06 16:13:09 +0800

    新增領域事件、錯誤處理及結果類別
    新增 `SanKalpa.Domain.Abstrations` 命名空間。
    在 `Entity.cs` 中新增抽象類別 `Entity`，包含領域事件管理。
    在 `Error.cs` 中新增記錄類別 `Error`，包含錯誤代碼和訊息。
    在 `Result.cs` 中新增類別 `Result` 及泛型類別 `Result<TValue>`，用於表示操作結果。

---
+++ src/SanKalpa.Domain/Abstrations/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace SanKalpa.Domain.Abstrations;
+
+public record class Error(string Code, string Message)
+{
+    public static Error None = new(string.Empty, string.Empty);
+    public static Error NullValue = new("Error.NullValue", "Failure result was provided null value");
+}
